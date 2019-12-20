51fbdaec28eb8951524b51433b40f86857745669
Sammy Uyghur <sammy.uyghur@ericsson.com>
2019-12-20 16:55:35 +0800

    Dynamically generated elements complete

--- js/index.js
+++ js/index.js
@@ -54,23 +54,36 @@ function settingTime() {
     dayShow.innerHTML = time[1];        
 }
 
-function todoAdded() {  
-    let input = $("#input-todo"); 
-    if (input.val == "") {
-        alert("hi")
-    }
-    
-    input.oninput = function () {
-        let btn = $(".btn-add");
-        btn[0].className = "btn-add btn-active";
-        
-    }
-    input.ini
-    
+$("#btn-add").onclick = function() {
+    let msg = $("#input-todo").value;
+    let childLi = document.createElement("li");
+    let childSpan = document.createElement("span");
+    let childSpan1 = document.createElement("span");
+    let childSpan2 = document.createElement("span");
+    let del_icon = document.createElement("i");
+    let list = $(".todo-list")[0];
+    let list_length = list.getElementsByTagName("li").length;
+    let itemId = "item" + list_length;
+    childLi.setAttribute("id",itemId);
+    $(".status-busy")[0].innerText = "You have " + (list_length + 1) + " pending items";
+    childSpan1.className = "todo-text";
+    childSpan2.className = "delete";
+    childSpan2.setAttribute("id",itemId + "-del");
+    childSpan1.innerHTML = msg;
+    del_icon.className = "icon-bin2";
+    childSpan2.appendChild(del_icon);
+    childLi.appendChild(childSpan);                     
+    childLi.appendChild(childSpan1);    
+    childLi.appendChild(childSpan2);   
+    $(".todo-list")[0].appendChild(childLi);
 }
 
+
+
+
+
+
 settingTime();
-todoAdded();
 
 
 
